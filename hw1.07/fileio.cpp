d382b63e1aa6fe0ffd7095123aa5adac08c38d31
wmaahs <wmaahs@iastate.edu>
2024-04-03 17:35:13 -0500

    created classes for different files data types, starting to parse them

--- hw1.07/fileio.cpp
+++ hw1.07/fileio.cpp
@@ -1,16 +1,29 @@
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <fstream>
+
 #include "fileio.h"
+csv_file all_csv_files[NUM_FILES];
+pokemon poke_arr[NUM_POMEMON];
+moves moves_arr[NUM_MOVES];
+poke_moves poke_moves_arr[NUM_POKE_MOVES];
+experience experience_arr[NUM_EXPERIENCE];
+poke_species poke_species_arr[NUM_POKE_SPECIES];
+type_names type_names_arr[NUM_TYPE_NAMES];
+poke_stats poke_stats_arr[NUM_POKE_STATS];
+stats stats_arr[NUM_STATS];
+poke_types poke_types_arr[NUM_POKE_TYPES];
 
 /**
  * 
  * Looks for the database in two different locations
- * if it it finds the database, it then returns the specific file
+ * if it it finds the database, it then calls the
+ * parsing funciton for that specific file
  * if it fails to find the data base, returns 0
 */
-std::fstream *find_in_database(std::string filename)
+int find_in_database(std::string filename)
 {
     std::fstream cur_csv;
     //first try to open from the /share/cs327/
@@ -19,20 +32,29 @@ std::fstream *find_in_database(std::string filename)
     //found in /share/cs327/
     if(cur_csv.is_open()) {
 
+        //pokemon.csv
+        if(filename == "pokemon.csv") {
+
+        }
         
-
-
+        return 1;
     }
-    return &cur_csv;
+
+    //next try to open from the $HOME/.poke
+    char * home_env;
+    home_env = getenv("HOME");
+    cur_csv.open(strcat(home_env, "/.poke327/pokedex/pokedex/data/csv/") + filename + ".csv");
+    if(cur_csv.is_open()){
+
+        return 1;
+    }
+    
+    return 0;
 }
 
-void parse_print_csv(std::string filename)
-{
-    std::fstream cur_csv;
-    //try to find the file
-    cur_csv = find_in_database(filename);
+// void print_parsed_csv(std::string filename)
+// {
 
-
-}
+// }
 
 
--- hw1.07/fileio.h
+++ hw1.07/fileio.h
@@ -1,6 +1,116 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 
+#define NUM_FILES 9
+
+#define NUM_POMEMON 1092
+#define NUM_MOVES 10018
+#define NUM_POKE_MOVES 528239
+#define NUM_POKE_SPECIES 898
+#define NUM_EXPERIENCE 601
+#define NUM_TYPE_NAMES 193
+#define NUM_POKE_STATS 6552
+#define NUM_STATS 8
+#define NUM_POKE_TYPES 1675
 
 
+class csv_file {
+public:
+    std::string filename;
+};
+
+class pokemon : public csv_file {
+public:
+    int id;
+    std::string name;
+    int species_id;
+    int h;
+    int w;
+    int base_xp;
+    int order;
+    bool is_default;
+};
+
+class moves : public csv_file {
+public:
+    int id;
+    std::string name;
+    int gen_id;
+    int type_id;
+    int power;
+    int pp;
+    int accuracy;
+    int priority;
+    int target_id;
+    int damage_class_id;
+    int effect_id;
+    int effect_chance;
+    int contest_type_id;
+    int contest_effect_id;
+    int super_contest_effect_id;
+};
+class poke_moves : public csv_file {
+public:
+    int pokemon_id;
+    int version_group_id;
+    int moves_id;
+    int pokemon_move_method_id;
+    int level;
+    int order;
+};
+class poke_species : public csv_file {
+public:
+    int id;
+    std::string name;
+    int gen_id;
+    int evolves_from_species_id;
+    int evolution_chain_id;
+    int color_id;
+    int shape_id;
+    int habitat_id;
+    int gender_rate;
+    int capture_rate;
+    int base_happiness;
+    bool is_baby;
+    int hatch_counter;
+    bool has_gender_differences;
+    int growth_rate_id;
+    int forms_switchable;
+    bool is_legendary;
+    bool is_mythical;
+    int order;
+    int conquest_order;
+};
+class experience : public csv_file {
+public:
+    int growth_rate_id;
+    int level;
+    int xp;
+};
+class type_names : public csv_file {
+public:
+    int type_id;
+    int local_language_id;
+    std::string name;
+};
+class poke_stats : public csv_file {
+public:
+    int poke_id;
+    int type_id;
+    int slot;
+};
+class stats : public csv_file {
+public:
+    int id;
+    int damage_class_id;
+    std::string name;
+    bool is_battle_only;
+    int game_index;
+};
+class poke_types : public csv_file {
+public:
+    int pokemon_id;
+    int type_id;
+    int slot;
+};
 #endif

c8716f9bb1bdba239a5974707a1d69ed47e5688f
wmaahs <wmaahs@pyrite-n5.cs.iastate.edu>
2024-04-04 02:13:59 -0500

    created tarball

---
+++ maahs_will_assignment1.07/character.h
@@ -0,0 +1,65 @@
+#ifndef CHARACTER_H
+# define CHARACTER_H
+
+# include <cstdint>
+
+# include "pair.h"
+
+#define DIJKSTRA_PATH_MAX (INT_MAX / 2)
+#define NO_NPCS 50
+
+typedef enum __attribute__ ((__packed__)) movement_type {
+  move_hiker,
+  move_rival,
+  move_pace,
+  move_wander,
+  move_sentry,
+  move_explore,
+  move_swim,
+  move_pc,
+  num_movement_types
+} movement_type_t;
+
+typedef enum __attribute__ ((__packed__)) character_type {
+  char_pc,
+  char_hiker,
+  char_rival,
+  char_swimmer,
+  char_other,
+  num_character_types
+} character_type_t;
+
+extern const char *char_type_name[num_character_types];
+
+class character {
+ public:
+  virtual ~character() {}
+  pair_t pos;
+  char symbol;
+  int next_turn;
+  int seq_num;
+};
+
+class npc : public character {
+ public:
+  character_type_t ctype;
+  movement_type_t mtype;
+  int defeated;
+  pair_t dir;
+};
+
+class pc : public character {
+};
+
+/* character is defined in poke327.h to allow an instance of character
+ * in world without including character.h in poke327.h                 */
+
+int32_t cmp_char_turns(const void *key, const void *with);
+void delete_character(void *v);
+
+extern void (*move_func[num_movement_types])(character *, pair_t);
+
+int pc_move(char);
+bool is_pc(character *c);
+
+#endif
---
+++ maahs_will_assignment1.07/fileio.h
@@ -0,0 +1,130 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#define NUM_FILES 9
+
+#define NUM_POKEMON 1092
+#define NUM_MOVES 844
+#define NUM_POKE_MOVES 528238
+#define NUM_POKE_SPECIES 898
+#define NUM_EXPERIENCE 601
+#define NUM_TYPE_NAMES 193
+#define NUM_POKE_STATS 6552
+#define NUM_STATS 8
+#define NUM_POKE_TYPES 1675
+
+
+class csv_file {
+public:
+    std::string filename;
+};
+
+class pokemon : public csv_file {
+public:
+    int id;
+    std::string name;
+    int species_id;
+    int h;
+    int w;
+    int base_xp;
+    int order;
+    int is_default;
+};
+
+class moves : public csv_file {
+public:
+    int id;
+    std::string name;
+    int gen_id;
+    int type_id;
+    int power;
+    int pp;
+    int accuracy;
+    int priority;
+    int target_id;
+    int damage_class_id;
+    int effect_id;
+    int effect_chance;
+    int contest_type_id;
+    int contest_effect_id;
+    int super_contest_effect_id;
+};
+class poke_moves : public csv_file {
+public:
+    int pokemon_id;
+    int version_group_id;
+    int moves_id;
+    int pokemon_move_method_id;
+    int level;
+    int order;
+};
+class poke_species : public csv_file {
+public:
+    int id;
+    std::string name;
+    int gen_id;
+    int evolves_from_species_id;
+    int evolution_chain_id;
+    int color_id;
+    int shape_id;
+    int habitat_id;
+    int gender_rate;
+    int capture_rate;
+    int base_happiness;
+    int is_baby;
+    int hatch_counter;
+    int has_gender_differences;
+    int growth_rate_id;
+    int forms_switchable;
+    int is_legendary;
+    int is_mythical;
+    int order;
+    int conquest_order;
+};
+class experience : public csv_file {
+public:
+    int growth_rate_id;
+    int level;
+    int xp;
+};
+class type_names : public csv_file {
+public:
+    int type_id;
+    int local_language_id;
+    std::string name;
+};
+class poke_stats : public csv_file {
+public:
+    int poke_id;
+    int stat_id;
+    int base_stat;
+    int effort;
+};
+class stats : public csv_file {
+public:
+    int id;
+    int damage_class_id;
+    std::string name;
+    int is_battle_only;
+    int game_index;
+};
+class poke_types : public csv_file {
+public:
+    int poke_id;
+    int type_id;
+    int slot;
+};
+
+void print_parsed_csv(std::string filename);
+void parse_pokemon(char * line, int line_ind);
+void parse_moves(char * line, int line_ind);
+void parse_poke_moves(char * line, int line_ind);
+void parse_experience(char * line, int line_ind);
+void parse_poke_species(char * line, int line_ind);
+void parse_type_names(char * line, int line_ind);
+void parse_poke_stats(char * line, int line_ind);
+void parse_stats(char * line, int line_ind);
+void parse_poke_types(char * line, int line_ind);
+
+void find_in_database(std::string filename);
+#endif
